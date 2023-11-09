0cef5527be8adae71ae773a8fe8b4affb4854c7d
Administrator <284812094@qq.com>
2023-11-09 11:20:23 +0800

    添加python文件夹和test_pytest.py文件

---
+++ python/calc.py
@@ -0,0 +1,5 @@
+class Cals:
+    def add(self,a,b):
+        return a + b
+    def div(self,a,b):
+        return a / b
---
+++ test/test_pytest.py
@@ -0,0 +1,15 @@
+import pytest
+
+from python.calc import Cals
+class TestCalc:
+    def setup(self):
+        self.cals=Cals()
+    def test_add(self):
+        result=self.cals.add(1,2)
+        assert 3 == result
+    def test_div(self):
+        result=self.cals.div(2,2)
+        assert  1 == result
+
+if __name__=='__main__':
+    pytest.main(['-vs','test_pytest.py::TestCalc::test_div'])
