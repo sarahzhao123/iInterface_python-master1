19e92b831f01f7067ae97c48b2817cf6dfc81fbe
Administrator <284812094@qq.com>
2023-11-09 13:09:21 +0800

    z在est_pytest.py文件里增加一条用例

--- test/test_pytest.py
+++ test/test_pytest.py
@@ -10,6 +10,9 @@ class TestCalc:
     def test_div(self):
         result=self.cals.div(2,2)
         assert  1 == result
+    def test_div1(self):
+        result=self.cals.div(2,2)
+        assert  1 == result
 
 if __name__=='__main__':
     pytest.main(['-vs','test_pytest.py::TestCalc::test_div'])
